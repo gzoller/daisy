86e26d5f4b9699c63482f3c31295a582f03fd118
Greg Zoller <greg.zoller@bottlerocketapps.com>
2014-05-28 12:52:02 -0500

    Finsihed proxy

--- ngx_http_daisy_module.c
+++ ngx_http_daisy_module.c
@@ -13,8 +13,6 @@
 
 	extern ngx_module_t  ngx_http_proxy_module;
 
-	//static char* ngx_http_daisy_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
-
 	extern ngx_module_t  *ngx_modules[];
 
 	static ngx_command_t  ngx_http_daisy_commands[] = {
@@ -40,8 +38,7 @@
 	    NULL,  /* merge server configuration */
 
 	    NULL,  /* create location configuration */
-//	    ngx_http_daisy_merge_loc_conf   /* merge location configuration */
-NULL
+	    NULL
 	};
 
 	ngx_module_t  ngx_http_daisy_module = {
@@ -59,50 +56,32 @@ NULL
 	    NGX_MODULE_V1_PADDING
 	};
 
-/*
-	static ngx_int_t
-	ngx_http_daisy_handler(ngx_http_request_t *r)
-	{
-		ngx_buf_t *b;
-		ngx_chain_t out;
-
-		char uriKey[256] = "";
-		if( r->uri.len == 0 ) {
-		    strcat(uriKey,"/");
-		} else {
-		    unsigned int len = 1;
-		    while( len != 0 && len < r->uri.len && len < 255 && r->uri.data[len] != '/' ) len++;
-		    strncat( uriKey, (const char*) r->uri.data, len);
-		}
-
-		char c[255] = "OK ";
-		strcat( c, uriKey );
-
-		r->headers_out.status = NGX_HTTP_OK;
-		r->headers_out.content_length_n = strlen(c);
-		r->headers_out.content_type.len = sizeof("text/html") - 1;
-		r->headers_out.content_type.data = (u_char*) "text/html";
-		ngx_http_send_header(r);
-
-		b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-		if( b == NULL ) {
-		    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Failed to allocate response buffer.");
-        	    return NGX_HTTP_INTERNAL_SERVER_ERROR;
-		}
-		b->pos = (unsigned char*)c;
-		b->last = (unsigned char*)c + strlen(c);
-		b->memory = 1;
-		b->last_buf = 1;
-		out.buf = b;
-		out.next = NULL;
-		return ngx_http_output_filter(r,&out);
-	}
-*/
-
  	void
 	getDynamicURL(ngx_str_t uri, int *len, unsigned char *urlBuf){
-		strcpy((char*)urlBuf,(const char*)"www.ford.com");
-		*len = 12;
+		loc_t *loc = NULL;
+
+		int scan = 0;
+		int count = 0;
+		while( count < 3 && scan < (int) uri.len ) {
+			if(uri.data[scan] == '/') count++;
+			scan++;
+		}
+		if( count == 3 ) {
+			scan--;
+			uri.data[scan] = '\0';
+			loc = lookup( (char*) uri.data );
+			uri.data[scan] = '/';
+			if( loc != NULL ) {
+				strcpy((char*)urlBuf, loc->ipFwd[loc->idx]);
+				*len = strlen( (char*)urlBuf );
+				//uri.data = &uri.data[scan];
+				//uri.len = uri.len - scan;
+	
+				// rotate the pointer
+				loc->idx += 1;
+				if( loc->idx == loc->fwdCount ) loc->idx = 0;
+			}
+		}
 	}
 
 	static char *
@@ -110,10 +89,6 @@ NULL
 	{
 	    ngx_str_t *value;
 	    value = cf->args->elts;
-
-	    //ngx_http_core_loc_conf_t *clcf;
-	    //clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	    //clcf->handler = ngx_http_daisy_handler;
 
 	    if( !ngx_strcmp(value[1].data, CMD_DB) ) {
 		// daisy db <db_name> <db_collection>
@@ -136,31 +111,10 @@ NULL
 		}
 	    }
 
-ngx_http_proxy_loc_conf_t *plcf;
-plcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_proxy_module);
-plcf->getDynamicURL = getDynamicURL;
-
-/*
-int i=0;
-for (i = 0; ngx_modules[i]; i++) {
-if( ngx_modules[i]->type == NGX_HTTP_MODULE )
-printf("Module_ %ld %ld v%ld type %ld\n",ngx_modules[i]->index, ngx_modules[i]->ctx_index,ngx_modules[i]->version,ngx_modules[i]->type);
-}
-*/
-/*
- * Goal:  Find proxy module and locate the loc_conf object.  Then insert getDynamicURL function pointer into it.
- */
+		// Find proxy module and locate the loc_conf object.  Then insert getDynamicURL function pointer into it.
+		ngx_http_proxy_loc_conf_t *plcf;
+		plcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_proxy_module);
+		plcf->getDynamicURL = getDynamicURL;
 
 	    return NGX_CONF_OK;
 	}
-
-/*
-	static char *
-	ngx_http_daisy_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
-	{
-printf("CF: %ld\n",(unsigned long)cf->ctx);
-printf("Dad: %ld\n",(unsigned long)parent);
-printf("Kid: %ld\n",(unsigned long)child);
-		return NGX_CONF_OK;
-	}
-*/
